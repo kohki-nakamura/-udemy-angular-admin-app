2f79fd9b7d4ce6598841e01a32ac92835545f890
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-06-19 12:14:57 +0900

    50. HTTPクライアントのエラーハンドリング

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { Member } from './member';
 import { MessageService } from './message.service';
 import { MEMBERS } from './mock-members';
+import { catchError,  map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +18,11 @@ export class MemberService {
   ) { }
 
   getMembers(): Observable<Member[]> {
-    this.messageService.add('MemberService: 社員一覧データを取得しました');
-    return this.http.get<Member[]>(this.membersUrl);
+    return this.http.get<Member[]>(this.membersUrl)
+     .pipe(
+       tap(members => this.log('社員データを取得しました。')),
+       catchError(this.handleError<Member[]>('getMembers', []))
+     );
   };
 
   getMember(id: number) :Observable<Member | undefined> {
@@ -28,5 +32,15 @@ export class MemberService {
 
   private log(message: string) {
     this.messageService.add(`MemberService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} 失敗: ${error.message}`);
+
+      return of(result as T);
+    }
   }
 }
